fdac1dfd7efa2be381c5f61463e5b335716bf1d5
David Anderson <david.anderson@redhat.com>
2022-12-12 12:01:33 -0600

    add hello path

--- hellorest.cpp
+++ hellorest.cpp
@@ -1,35 +1,42 @@
 #include <memory>
+#include <string>
 #include <cstdlib>
 #include <restbed>
 
 using namespace std;
 using namespace restbed;
 
-void post_method_handler( const shared_ptr< Session > session )
+void hello( const shared_ptr< Session > session )
 {
     const auto request = session->get_request( );
 
-    int content_length = request->get_header( "Content-Length", 0 );
+    session->close( OK, "Hello World!\n" , { { "Content-Length", "13" } } );
+}
 
-    session->fetch( content_length, [ ]( const shared_ptr< Session > session, const Bytes & body )
-    {
-        fprintf( stdout, "%.*s\n", ( int ) body.size( ), body.data( ) );
-        session->close( OK, "Hello, World!", { { "Content-Length", "13" } } );
-    } );
+void helloname( const shared_ptr< Session > session )
+{
+    const auto request = session->get_request( );
+
+    string responseText = "Hello, " + request->get_path_parameter("name") + "!\n";
+    session->close( OK, responseText , { { "Content-Length", ::to_string( responseText.size() ) } } );
 }
 
 int main( const int, const char** )
 {
-    auto resource = make_shared< Resource >( );
-    resource->set_path( "/resource" );
-    resource->set_method_handler( "POST", post_method_handler );
+    auto hello_resource = make_shared< Resource >( );
+    hello_resource->set_path( "/hello" );
+    hello_resource->set_method_handler( "GET", hello );
+    auto helloname_resource = make_shared< Resource >( );
+    helloname_resource->set_path( "/hello/{name: .*}" );
+    helloname_resource->set_method_handler( "GET", helloname );
 
     auto settings = make_shared< Settings >( );
     settings->set_port( 9080 );
     settings->set_default_header( "Connection", "close" );
 
     Service service;
-    service.publish( resource );
+    service.publish( hello_resource );
+    service.publish( helloname_resource );
     service.start( settings );
 
     return EXIT_SUCCESS;
